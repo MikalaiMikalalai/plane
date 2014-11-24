744a5e2a06bab958d01e0af6b8f8b67d769250bd
MikalaiMikalalai <mikalaimikalalai@gmail.com>
2014-11-24 22:51:29 +0300

    Animation and basic control added
    Conflicts:
    	core/src/com/mygdx/game/MyGdxGame.java

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,39 +2,103 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL20;	
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
 	Texture img;
+	private Animation standingAnimation;
+	private TextureRegion currentFrame;
+	private float stateTime;
+	private Animation currentAnimation;
+	private Animation jumpingAnimation;
+	private Animation kickAnimation;
+	private Animation runningAnimation;
+	private float positionX;
+	private float positionY;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture(Gdx.files.internal("hero_sprites.png"));
+		TextureRegion[][] regions = new TextureRegion(img).split(img.getWidth()/6, img.getHeight()/4);
+		TextureRegion[] standingAnimationRegion = new TextureRegion[5];
+		
+		for(int i = 0; i<5; i++){
+			standingAnimationRegion[i] = regions[0][i];
+		}
+		
+		TextureRegion[] jumpingAnimationRegion = new TextureRegion[5];
+		
+		for(int i = 0; i<5; i++){
+			jumpingAnimationRegion[i] = regions[1][i];
+		}
+		
+		
+		TextureRegion[] runningAnimationRegion = new TextureRegion[6];
+		
+		for(int i = 0; i<6; i++){
+			runningAnimationRegion[i] = regions[2][i];
+		}
+		
+		TextureRegion[] kickAnimationRegion = new TextureRegion[5];
+		
+		for(int i = 0; i<5; i++){
+			kickAnimationRegion[i] = regions[3][i];
+		}
+		
+		standingAnimation = new Animation(0.100f, standingAnimationRegion);
+		jumpingAnimation = new Animation(0.100f, jumpingAnimationRegion);
+		kickAnimation = new Animation(0.100f, kickAnimationRegion);
+		runningAnimation = new Animation(0.100f, runningAnimationRegion);
+		
+		currentAnimation = standingAnimation;
+		Gdx.input.setInputProcessor(this);
+		stateTime = 0f;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stateTime += Gdx.graphics.getDeltaTime();
+		
+		currentFrame = currentAnimation.getKeyFrame(stateTime, true); 
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(currentFrame, positionX, 0);
 		batch.end();
 	}
 
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
-		return false;
+		switch(keycode){
+		case Keys.A: currentAnimation = runningAnimation;
+					 positionX -= 200f * Gdx.graphics.getDeltaTime();
+					 break;
+		case Keys.D: currentAnimation = runningAnimation;
+		 			 positionX += 200f * Gdx.graphics.getDeltaTime();
+		 			 break;
+		case Keys.SPACE: currentAnimation = jumpingAnimation;
+					 break;
+		case Keys.CONTROL_LEFT:
+		case Keys.CONTROL_RIGHT: currentAnimation = kickAnimation;
+					 break;
+		 			 
+		}
+		return true;
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
 		// TODO Auto-generated method stub
+		currentAnimation = standingAnimation;		
 		return false;
 	}
 
