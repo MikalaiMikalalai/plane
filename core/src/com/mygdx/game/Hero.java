bb689c0588ac7bf37a224a2f91d5a67f2a27a172
MikalaiMikalalai <mikalaimikalalai@gmail.com>
2015-04-09 09:01:41 +0300

    organize imports

--- core/src/com/mygdx/game/Hero.java
+++ core/src/com/mygdx/game/Hero.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.mygdx.game.GameCharacter.Direction;
 
 public class Hero extends GameCharacter {
 
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,23 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;	
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.mygdx.game.GameCharacter.Direction;
 
 public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	
 	SpriteBatch spriteBatch;	
 	private float ellapsedGameTime;
-	private float positionX;
-	private float positionY;
-	private TextureRegion[][] regions;
 	private Hero myHero;	
 	
 	@Override
